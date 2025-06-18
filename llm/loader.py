b8663430d04b5588de9e5753ba9433b195f58cad
adeptus-kanish <kanish@underpinservices.com>
2025-06-18 16:54:14 +0530

    fix: suppress verbose for metal backend logs

--- llm/loader.py
+++ llm/loader.py
@@ -1,21 +1,36 @@
 from llama_cpp import Llama
 from utils.prompt_builder import build_prompt
 import re
+import os
+import sys
+import contextlib
 
 MODEL_PATH = "llm/model/mistral-7b-instruct-v0.3-q4_k_m.gguf"
 
+# suppress verbose for Metal backend logs (llama.cpp backend)
+@contextlib.contextmanager
+def suppress_stderr():
+    with open(os.devnull, 'w') as devnull:
+        old_stderr = sys.stderr
+        sys.stderr = devnull
+        try:
+            yield
+        finally:
+            sys.stderr = old_stderr
+
 class LLMQueryEngine:
     def __init__(self, model_path=MODEL_PATH):
-        self.model = Llama(
-            model_path=model_path,
-            n_ctx=512,
-            n_gpu_layers=-1,  # Use GPU (M2 Metal) if running out of VRAM use 20
-            n_threads=6,
-            n_batch=64,
-            use_mlock=True,
-            use_mmap=True,
-            verbose=False   # Use True for debugging
-        )
+        with suppress_stderr():
+            self.model = Llama(
+                model_path=model_path,
+                n_ctx=1024,
+                n_gpu_layers=-1,  # Use GPU (M2 Metal) if running out of VRAM use 20
+                n_threads=6,
+                n_batch=64,
+                use_mlock=True,
+                use_mmap=True,
+                verbose=False   # Use True for debugging
+            )
 
     def clean_sql(self, raw_output):
         match = re.search(r"(?i)(select|insert|update|delete).*?;", raw_output, re.DOTALL)
